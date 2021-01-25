60134d2ad63d58af2d3df00faeb57ffbd3ade19b
SantamRC <santamdev404@gmail.com>
2021-01-25 20:06:32 +0530

    models

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::fs::File;
+use rust_sbml::Model;
+
+fn main(){
+    let mut file = File::open("./sbml files/enzymekinetics.xml").expect("Unable to open the file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read the file");
+
+    let result = Model::parse(&contents);
+    println!("{:?}", result.unwrap());
 }
