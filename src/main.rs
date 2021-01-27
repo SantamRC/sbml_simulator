a3766fa4b7a68996365f6ccf3aae145e6e6f2862
SantamRC <santamdev404@gmail.com>
2021-01-27 21:12:47 +0530

    species

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use std::io::prelude::*;
 use std::fs::File;
 use rust_sbml::Model;
 
+
 fn main(){
     let mut file = File::open("./sbml files/enzymekinetics.xml").expect("Unable to open the file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Unable to read the file");
 
     let result = Model::parse(&contents);
-    println!("{:?}", result.unwrap());
+    println!("{:?}", result.unwrap().get_list_of_species());
+
+   
 }
